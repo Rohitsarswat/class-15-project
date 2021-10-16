37ecfc8363efafa129df1f1545e822d688b34d30
Rohitsarswat <83912817+Rohitsarswat@users.noreply.github.com>
2021-10-16 18:19:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -79,9 +79,9 @@ function draw() {
       if(swordGroup.isTouching(boy)) {
         gameState=END;
         
-        boy.addAnimation(endImg);
-        // boy.addAnimation("SahilRunning",endImg);
-        // boy.addAnimation("SahilRunning");
+        //boy.addAnimation(endImg);
+         boy.addAnimation("SahilRunning",endImg);
+       //  boy.addAnimation("SahilRunning");
         // boy.addAnimation(SahilRunning,endImg);
 
         boy.x=200;
